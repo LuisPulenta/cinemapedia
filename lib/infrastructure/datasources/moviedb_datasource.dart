3835c91c44dda0977986e857b488261512a85b75
LuisPulenta <luisalbertonu@gmail.com>
2023-05-04 10:56:44 -0300

    Video 227

--- lib/infrastructure/datasources/moviedb_datasource.dart
+++ lib/infrastructure/datasources/moviedb_datasource.dart
@@ -84,4 +84,15 @@ class MoviedbDatasource extends MoviesDatasource {
 
     return movie;
   }
+
+//---------------------------------------------------------------
+//----------------------- searchMovies --------------------------
+//---------------------------------------------------------------
+
+  @override
+  Future<List<Movie>> searchMovies(String query) async {
+    final response =
+        await dio.get('/search/movie', queryParameters: {'query': query});
+    return _jsonToMovies(response.data);
+  }
 }
---
+++ lib/presentation/delegates/search_movie_delegate.dart
@@ -0,0 +1,71 @@
+import 'package:animate_do/animate_do.dart';
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:flutter/material.dart';
+
+typedef SearchMoviesCallback = Future<List<Movie>> Function(String query);
+
+class SearchMovieDelegate extends SearchDelegate<Movie?> {
+  final SearchMoviesCallback searchMovies;
+
+  SearchMovieDelegate({required this.searchMovies});
+
+  @override
+  String get searchFieldLabel => 'Buscar película...';
+
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    return [
+      FadeIn(
+        animate: query.isNotEmpty,
+        duration: const Duration(milliseconds: 400),
+        child: IconButton(
+          icon: const Icon(
+            Icons.cancel,
+            color: Colors.red,
+          ),
+          onPressed: () {
+            query = '';
+          },
+        ),
+      ),
+    ];
+  }
+
+  @override
+  Widget? buildLeading(BuildContext context) {
+    return IconButton(
+      icon: const Icon(
+        Icons.arrow_back_ios_outlined,
+        color: Colors.black,
+      ),
+      onPressed: () {
+        close(context, null);
+      },
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return const Text('buildResults');
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    return FutureBuilder(
+      future: searchMovies(query),
+      builder: (context, snapshot) {
+        final movies = snapshot.data ?? [];
+
+        return ListView.builder(
+          itemCount: movies.length,
+          itemBuilder: (context, index) {
+            final movie = movies[index];
+            return ListTile(
+              title: Text(movie.title),
+            );
+          },
+        );
+      },
+    );
+  }
+}
