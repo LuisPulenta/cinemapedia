6c90d791bef75bad0fbb0f50136265eeb0b7e305
LuisPulenta <luisalbertonu@gmail.com>
2023-05-06 15:35:39 -0300

    Video 269

--- lib/presentation/views/movies/favorites_view.dart
+++ lib/presentation/views/movies/favorites_view.dart
@@ -1,4 +1,5 @@
 import 'package:cinemapedia/presentation/providers/providers.dart';
+import 'package:cinemapedia/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -19,17 +20,6 @@ class FavoritesViewState extends ConsumerState<FavoritesView> {
   Widget build(BuildContext context) {
     final favoritesMovies = ref.watch(favoriteMoviesProvider).values.toList();
 
-    return Scaffold(
-      body: ListView.builder(
-        itemCount: favoritesMovies.length,
-        itemBuilder: (context, index) {
-          final movie = favoritesMovies[index];
-          return ListTile(
-              title: Text(
-            movie.title,
-          ));
-        },
-      ),
-    );
+    return Scaffold(body: MovieMasonry(movies: favoritesMovies));
   }
 }
